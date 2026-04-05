8dd7678630840cf5cce09902f04147af8b2784ea
Nuno_Leite <nunoleite144@gmail.com>
2026-04-05 15:39:25 +0100

    fix RCOM

--- include/application.h
+++ include/application.h
@@ -3,6 +3,15 @@
 
 #include "link_layer.h"
 
+// Packet types
+#define PACKET_START 1
+#define PACKET_END 2
+#define PACKET_DATA 3
+
+// TLV types
+#define TLV_SIZE 0
+#define TLV_NAME 1
+
 // (tx ou rx)
 int application(const char *serialPort, const char *role, const char *filename);
 
